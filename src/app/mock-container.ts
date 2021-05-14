4dd669617e2201e6df7ab5c99de03355973c368d
Benedikt <benedikt2302@hotmail.de>
2021-05-14 09:29:59 +0200

    Rename Mock Containers.

--- src/app/mock-container.ts
+++ src/app/mock-container.ts
@@ -1,8 +1,8 @@
 import { ContainerItem } from './container-item';
 
 export const CONTAINER: ContainerItem[] = [
-  { id: 1, name: 'Kiste 1', location: 'Stube'},
-  { id: 2, name: 'Kiste 2', location: 'Stube'},
-  { id: 3, name: 'Kiste 3', location: 'Stube'},
-  { id: 4, name: 'Kiste 4', location: 'Stube'}
+  { id: 1, name: 'Container 1', location: 'xyz'},
+  { id: 2, name: 'Container 2', location: 'xyz'},
+  { id: 3, name: 'Container 3', location: 'xyz'},
+  { id: 4, name: 'Container 4', location: 'xyz'}
 ];
